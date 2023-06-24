15e0f4a47a4ecb592a0796cdd57a85a18f46d31f
Victor <eduardovictor822@gmail.comm>
2023-06-24 10:31:17 -0300

    Flask Imersao

---
+++ meusite.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from random import randint
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    variavel = "Game: Advinhe o número correto"
+
+    if request.method == "GET": 
+        return render_template("index.html", variavel = variavel)
+    
+    else: 
+        numero = randint(1,10)
+        palpite = int(request.form.get("name"))
+        if numero == palpite:
+            return '<h1>Resultado Correto</h1>'
+        else: 
+            return '<h1>Resultado Errado</h1>'
+
+
+@app.route('/<string:nome>')
+def error(nome):
+    variavel = f'Página ({nome}) não existe'
+    return render_template("error.html", variavel2=variavel)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
